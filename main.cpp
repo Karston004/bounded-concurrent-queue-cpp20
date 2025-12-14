caffeaa8b5ad7a66b6c5efbfc2659b9e71b541bb
Joe Smith <004jsmith@gmail.com>
2025-12-14 02:12:08 +0000

    Fix: made constructor explicit

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ private:
     std::mutex tailLock_;
 
 public:
-    BoundedQueue(int capacity) : addable_(capacity > 0 ? capacity: throw std::invalid_argument("capacity must be > 0")), removable_(0) {
+    explicit BoundedQueue(int capacity) : addable_(capacity > 0 ? capacity: throw std::invalid_argument("capacity must be > 0")), removable_(0) {
         head = nullptr;
         tail = nullptr;
     }
